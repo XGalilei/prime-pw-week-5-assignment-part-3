799e281d68dd37d21fd0f5e97d06922662ad6ab6
XGalilei <ianbush1@gmail.com>
2021-08-10 13:44:54 -0500

    added in collection and addToCollection method

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,12 @@
 console.log('***** Music Collection *****')
+let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+  let album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  };
+  collection.push(album);
+  return album;
+}
