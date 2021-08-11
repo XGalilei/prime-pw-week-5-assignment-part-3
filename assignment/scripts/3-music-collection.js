02188c5b0b1b8766419a1e21b1aa7c7e7cfd7571
XGalilei <ianbush1@gmail.com>
2021-08-11 10:56:52 -0500

    Added in all six albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,9 +12,13 @@ function addToCollection(title, artist, yearPublished) {
 }
 
 function showCollection() {
-  
+
 }
 
-console.log(addToCollection('The Gereg', 'The Hu', 2019));
-console.log(addToCollection('Revolution Radio', 'Green Day', 2016));
+console.log(addToCollection('The Gereg', 'The Hu', 2019)); //first album
+console.log(addToCollection('Revolution Radio', 'Green Day', 2016)); //second album
+console.log(addToCollection('Save Rock and Roll', 'Fall Out Boy', 2013)); //third album
+console.log(addToCollection('American Idiot', 'Green Day', 2004)); //fourth album
+console.log(addToCollection('N.A.T.I.O.N', 'Bad Wolves', 2019)); //fifth album
+console.log(addToCollection('Folie a Deux', 'Fall Out Boy', 2008)); //sixth album
 console.log(collection);
